976396fd7dbcbe6571c151846cc99eacc00c7e15
VladimirPopkov <vovanpopkov@mail.ru>
2020-11-21 15:06:06 +0300

    Поправил ошибку

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Person people[] = new Person[50];
         Random random = new Random();
         for(int i = 0; i < people.length; i++){
-            people[i] = new Person(firstName[random.nextInt(5) - 0], lastName[random.nextInt(6) - 0]);
+            people[i] = new Person(firstName[random.nextInt(5)], lastName[random.nextInt(6)]);
             System.out.println("Человек №" + (i + 1) + " - " + people[i].toString());
         }
     }
